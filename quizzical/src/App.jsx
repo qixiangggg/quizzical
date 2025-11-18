48cc110dadeb9d40a25931c9eb32f95d0009fdeb
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-11-18 21:32:31 +0800

    Setup homepage html

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Homepage from './components/Homepage'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Homepage/>
     </>
   )
 }
---
+++ quizzical/src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+export default function Homepage(){
+    return(
+        <main>
+        <h1>Quizzical</h1>
+        <p>Some description if needed</p>
+        <button>Start quiz</button>
+        </main>
+    )
+}
