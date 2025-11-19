82511f2ac5e1928a9b323af2d1b38a2a69fa3972
desmond.chong <desmond.chong@beans.com.my>
2025-11-19 11:43:40 +0800

    fetch quiz data

--- quizzical/src/components/Quizpage.jsx
+++ quizzical/src/components/Quizpage.jsx
@@ -1,4 +1,15 @@
+import { useEffect,useState } from "react"
 export default function Quizpage(){
+    const [quizData, setQuizData] = useState([])
+    useEffect(() => {
+        async function getData(){
+            const response = await fetch("https://opentdb.com/api.php?amount=5")
+            const data = await response.json()
+            setQuizData(data.results)
+        }
+        getData()
+    },[])
+    console.log(quizData)
     return(
         <h1>test</h1>
     )
