d759322ef80b400b54a6dd6dd502d0edeb32a81c
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-11-19 21:57:15 +0800

    Insert correct answer randomly into options

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -3,7 +3,9 @@ import Option from "./Option"
 import { v4 as uuidv4 } from "uuid"
 import { clsx } from "clsx"
 export function Quiz(props){
-    const options = [...props.quiz.incorrect_answers, props.quiz.correct_answer]
+    const options = [...props.quiz.incorrect_answers]
+    const randomIndex = Math.ceil(Math.random() * options.length)
+    options.splice(randomIndex, 0, props.quiz.correct_answer)
     
     const optionElements = options.map(option => {
         const selectedAnswer = props.selectedAnswer
