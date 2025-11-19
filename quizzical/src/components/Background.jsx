01c3434c5ad498f3ee69d43a8b94228e0732c397
desmond.chong <desmond.chong@beans.com.my>
2025-11-19 09:16:05 +0800

    Styling homepage

---
+++ quizzical/src/components/Background.jsx
@@ -0,0 +1,7 @@
+export default function Background(props){
+    return(
+        <>
+            <img src={props.img} className={props.class}/>
+        </>
+    )
+}
--- quizzical/src/components/Homepage.jsx
+++ quizzical/src/components/Homepage.jsx
@@ -1,9 +1,17 @@
+import Background from './Background'
+import blueBubble from "../assets/blue-bubble.png"
+import yellowBubble from "../assets/yellow-bubble.png"
+
 export default function Homepage(){
     return(
-        <main>
-        <h1>Quizzical</h1>
-        <p>Some description if needed</p>
-        <button>Start quiz</button>
-        </main>
+        <>
+            <Background img={yellowBubble} class="top-bubble"/>
+            <main className="homepage">
+                <h1>Quizzical</h1>
+                <p>Some description if needed</p>
+                <button>Start quiz</button>
+            </main>
+            <Background img={blueBubble} class="bottom-bubble"/>
+        </>
     )
 }
